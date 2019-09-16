e80e72ce15b688a165ce7a560f2399b55963782c
agustin94 <agustin@theeye.io>
2019-09-15 23:59:14 -0300

    test

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const puppeteer = require('puppeteer');
+
 //const CRED_PATH = require ('./credentials.json');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false,args: ['--start-maximized'] })
+  
+  const browser = await puppeteer.launch({ headless: true,args: ['--start-maximized'] })
   const page = await browser.newPage()
 
-  await page.goto('https://seti.afip.gob.ar/padron-puc-constancia-internet/ConsultaConstanciaAction.do',{waitUntil: 'networkidle0'})
+  await page.goto('https://seti.afip.gob.ar/padron-puc-constancia-internet/ConsultaConstanciaAction.do')
   await page.setViewport({ width : 1366, height : 657 })
 
   const elementHandle = await page.$('iframe[id="miBody"]');
   const frame = await elementHandle.contentFrame();
-  await frame.waitForSelector('#cuit')
-  await frame.type('#cuit',"20-38403239-8")
-
+  await frame.waitForSelector('#captchaField')
+  await frame.click('#captchaField')
+  await page.screenshot({path: 'buddy-screenshot.png',fullPage: true});
+    
+  //await frame.type('#cuit',"20-38403239-8")
+  browser.close();
  })();
